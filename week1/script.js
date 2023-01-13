99b7082dbc7767d009f3410203ba61fbdaf2fbc2
Julio Ramos <64497770+j-ramos-css@users.noreply.github.com>
2023-01-13 15:38:28 -0800

    index and script

---
+++ week1/script.js
@@ -0,0 +1,4 @@
+const firstName = "Julio";
+const lastName = "Ramos";
+let yearsOfStudy = 2;
+let goal = "freelance while beginning my job search into web development and UI. I may also continue some education into more advanced languages and frameworks ";
